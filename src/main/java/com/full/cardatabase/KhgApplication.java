2d1b28fb31d411b66d662531bccc72a58afb27b4
khg <winquake3@hanmail.net>
2025-08-15 03:09:51 +0900

    Car 테이블 생성까지

--- src/main/java/com/full/cardatabase/KhgApplication.java
+++ src/main/java/com/full/cardatabase/KhgApplication.java
@@ -1,4 +1,4 @@
-package com.full.khg;
+package com.full.cardatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/full/cardatabase/controller/HomeController.java
+++ src/main/java/com/full/cardatabase/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.full.khg.controller;
+package com.full.cardatabase.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
