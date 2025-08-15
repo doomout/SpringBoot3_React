542c2ab86cddc27c3b6b4f1b51e8a6413984de1f
khg <winquake3@hanmail.net>
2025-08-15 12:36:36 +0900

    테스트 데이터 입력, 전체 조회까지

---
+++ src/main/java/com/full/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,36 @@
+package com.full.cardatabase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.full.cardatabase.domain.Car;
+import com.full.cardatabase.domain.CarRepository;
+
+@SpringBootApplication
+public class CardatabaseApplication implements CommandLineRunner{
+
+	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
+
+	private final CarRepository repository;
+	public CardatabaseApplication(CarRepository repository) {
+		this.repository = repository;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000));
+		repository.save(new Car("Nissan", "Leaf", "white", "SSJ-3002", 2020, 29000));
+		repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000));
+
+		//모든 자동차를 가져와 Console에 로깅
+		for(Car car : repository.findAll()) {
+			logger.info("brand: {}, model: {}", car.getBrand(), car.getModel());
+		}
+	}
+}
