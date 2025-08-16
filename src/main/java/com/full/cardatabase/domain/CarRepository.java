34b5f78e81a8eaaac3476ead0dda18fec4fa7666
khg <winquake3@hanmail.net>
2025-08-16 23:43:16 +0900

    자동차 간단 검색 기능 추가

--- src/main/java/com/full/cardatabase/domain/CarRepository.java
+++ src/main/java/com/full/cardatabase/domain/CarRepository.java
@@ -1,7 +1,25 @@
 package com.full.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
 
 public interface CarRepository extends CrudRepository<Car, Long> {
+    // 브랜드로 자동차 검색
+    List<Car> findByBrand(String brand);
     
+    // 색상으로 자동차 검색
+    List<Car> findByColor(String color);
+    
+    // 연도로 자동차 검색
+    List<Car> findByModelYear(int modelYear);
+
+    // 브랜드와 모델로 자동차를 검색
+    List<Car> findByBrandAndModel(String brand, String model);
+
+    // 브랜드 또는 색상으로 자동차 가져오기
+    List<Car> findByBrandOrColor(String brand, String color);
+
+    // 브랜드로 자동차를 검색하고 연도로 정렬
+    List<Car> findByBrandOrderByModelYearAsc(String brand);
 }
