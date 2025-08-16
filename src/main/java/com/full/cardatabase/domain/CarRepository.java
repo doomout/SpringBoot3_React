0507a3ba5afd0416b9ce9750ab4804f99e72403f
khg <winquake3@hanmail.net>
2025-08-16 23:50:37 +0900

    SQL 문을 사용한 검색

--- src/main/java/com/full/cardatabase/domain/CarRepository.java
+++ src/main/java/com/full/cardatabase/domain/CarRepository.java
@@ -1,12 +1,13 @@
 package com.full.cardatabase.domain;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 
 public interface CarRepository extends CrudRepository<Car, Long> {
     // 브랜드로 자동차 검색
-    List<Car> findByBrand(String brand);
+    //List<Car> findByBrand(String brand);
     
     // 색상으로 자동차 검색
     List<Car> findByColor(String color);
@@ -22,4 +23,12 @@ public interface CarRepository extends CrudRepository<Car, Long> {
 
     // 브랜드로 자동차를 검색하고 연도로 정렬
     List<Car> findByBrandOrderByModelYearAsc(String brand);
+
+    // SQL 문을 사용한 브랜드로 자동차 검색
+    //@Query("select c from Car c where c.brand = ?1")
+    //List<Car> findByBrand(String brand);
+
+    // like 를 사용한 브랜드로 자동차 검색
+    @Query("select c from Car c where c.brand like %?1")
+    List<Car> findByBrand(String brand);
 }
