6b815e4955e434b4878c39619c3b3f5e648fef96
khg <winquake3@hanmail.net>
2025-08-21 18:24:18 +0900

    유저 정보가 노출되지 않도록 보안 강화

--- src/main/java/com/full/cardatabase/domain/AppUserRepository.java
+++ src/main/java/com/full/cardatabase/domain/AppUserRepository.java
@@ -3,7 +3,9 @@ package com.full.cardatabase.domain;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface AppUserRepository extends CrudRepository<AppUser, Long> {
     Optional<AppUser> findByUsername(String username);
 }
