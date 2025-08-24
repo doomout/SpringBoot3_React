91e0ab28c7fbc8be38d3e5378ce591b3ad3cf6d7
khg <winquake3@hanmail.net>
2025-08-25 04:22:33 +0900

    첫 테스트 코드 완성

--- src/main/java/com/full/cardatabase/SecurityConfig.java
+++ src/main/java/com/full/cardatabase/SecurityConfig.java
@@ -18,7 +18,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.*;
-import org.springframework.http.HttpMethod;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
