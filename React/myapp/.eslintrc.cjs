5d1a1f1737963266a25cd52805a03953ff4b6183
khg <winquake3@hanmail.net>
2025-08-30 01:38:24 +0900

    설정 추가

---
+++ React/myapp/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended", // prettier랑 연동
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": "off", // prop-types 안 쓸 거라면 꺼버리기
+  },
+};
