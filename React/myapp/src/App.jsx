d60e961d762599e28331f09334bf129f81e5251f
khg <winquake3@hanmail.net>
2025-08-29 16:41:49 +0900

    일괄 처리 예제

--- React/myapp/src/App.jsx
+++ React/myapp/src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.css'
 import Counter from "./Counter"; // Counter 컴포넌트 가져오기
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  
+  const increment = () =>{
+    setCount(count + 1);  // 아직 렌더링 안됨
+    setCount2(count2 + 1); 
+    // 모든 상태가 업데이트 되고 나서 컴포넌드 재랜더링
+  } 
+
   return (
     <>
-    <h1>Hello World</h1>
-    <h2>This is myfirst React component</h2>
-    <Counter />
+    <p>Counters: {count} {count2}</p>
+    <button onClick={increment}>Increment</button>
     </>
   );
 }
