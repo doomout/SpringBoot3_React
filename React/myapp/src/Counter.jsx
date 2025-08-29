1186a7412ae49bd928fe0160278fafd5cb62cb44
khg <winquake3@hanmail.net>
2025-08-30 01:38:46 +0900

    사용자 정의 훅 예제

--- React/myapp/src/Counter.jsx
+++ React/myapp/src/Counter.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
+import useTitle from "./UseTitle";
 
 function Counter() {
     // 초기 값이 0인 count 상태
     const [count, setCount] = useState(0);
+    useTitle(`You clicked ${count} times`);
+    
     return (
+    <>
         <div>
             <p>Counter = {count}</p>
             <button onClick={() => setCount(count + 1)}>
                 Increment
             </button>
         </div>
+    </>
     );
 };
 
