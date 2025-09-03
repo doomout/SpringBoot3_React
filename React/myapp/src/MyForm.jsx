5d256f444d2d365345df759c87281384661fe8d2
khg <winquake3@hanmail.net>
2025-09-03 22:47:45 +0900

    각각의 상태를 이용하여 구현

--- React/myapp/src/MyForm.jsx
+++ React/myapp/src/MyForm.jsx
@@ -1,36 +1,30 @@
 import { useState } from "react";
 
 function MyForm() {
-     const [user, setUser] = useState(
-        {
-            firstName: '',
-            lastName: '',
-            email: ''
-        }
-     )
-    //입력 요소의 내용이 변경되면 값을 상태에 저장
-    const handleChange = (event) => {
-        setUser({...user, [event.target.name]:event.target.value});
-    }
-
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+     
     const handleSubmit = (event) => {
-        alert(`Hello ${user.firstName} ${user.lastName} ${user.email}`);
+        alert(`Hello \n ${firstName} \n ${lastName}\n ${email}`);
         event.preventDefault();
     }
     return (
         <form onSubmit={handleSubmit}>
-            <p>
-            <label>First name</label>
-            <input type="text" name="firstName" onChange={handleChange} value={user.firstName}/>
-            </p>
-            <p>
-            <label>Last name</label>
-            <input type="text" name="lastName" onChange={handleChange} value={user.lastName}/>
-            </p>
-            <p>
-            <label>Email</label>
-            <input type="email" name="email" onChange={handleChange} value={user.email}/>
-            </p>
+            <label>First name </label>
+            <input  
+                onChange={e => setFirstName(e.target.value)} 
+                value={firstName}/><br/>
+            
+            <label>Last name </label>
+            <input  
+                onChange={e => setLastName(e.target.value)} 
+                value={lastName}/><br/>
+
+            <label>Email </label>
+            <input 
+                onChange={e => setEmail(e.target.value)} 
+                value={email}/><br/>
             <input type="submit" value="눌러봐"/>
         </form>
     );
