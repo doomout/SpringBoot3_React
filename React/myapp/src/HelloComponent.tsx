0ad831be275a4e4d94faac87f9e78124669414b8
khg <winquake3@hanmail.net>
2025-09-04 23:00:43 +0900

    화살표 함수형 컴포넌트로 변경

--- React/myapp/src/HelloComponent.tsx
+++ React/myapp/src/HelloComponent.tsx
@@ -1,6 +1,8 @@
-import { HelloProps } from "./types";
+import React from 'react';
+import { HelloProps } from './types';
 
-function HelloComponent({name, age}: HelloProps) {
+// 기존 코드를 화살표 함수형 컴포넌트로 변경
+const HelloComponent: React.FC<HelloProps> = ({name, age}) => {
     return (
         <>
         안녕 나는 {name}인데, 난 {age} 살이야.
