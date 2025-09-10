70155b8180a16598198cf8f4c3477d7c0d132cca
khg <winquake3@hanmail.net>
2025-09-11 04:09:05 +0900

    머티리얼  UI 컴포넌트 기초 코드

---
+++ React/shoppinglist/README.md
@@ -0,0 +1,12 @@
+### 머티리얼 UI 컴포넌트 이용 앱
+
+## 1. 환경설정
+
+```bash
+npm create vite@latest
+shoppinglist -> React -> TypeScript
+cd shoppinglist
+npm install
+# MUI 설치
+npm install @mui/material @emotion/react @emotion/styled
+```
---
+++ React/shoppinglist/src/App.tsx
@@ -0,0 +1,18 @@
+import Container from '@mui/material/Container';
+import './App.css'
+import { useState } from 'react';
+import  AppBar  from '@mui/material/AppBar';
+import  Toolbar  from '@mui/material/Toolbar';
+import  Typography  from '@mui/material/Typography';
+
+function App() {
+  return (
+    <>
+      <Container>
+
+      </Container>
+    </>
+  )
+}
+
+export default App
