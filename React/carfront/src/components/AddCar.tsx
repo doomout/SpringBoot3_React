eee3703904aabcc9e1d7c290f5ab434997ad9357
khg <winquake3@hanmail.net>
2025-09-17 23:21:12 +0900

    생성 기능 추가 - button, Dialog 추가

--- React/carfront/src/components/AddCar.tsx
+++ React/carfront/src/components/AddCar.tsx
@@ -16,8 +16,35 @@ function AddCar() {
         price: 0
     });
 
+    // 모달 폼 열기
+    const handleClickOpen = () => {setOpen(true)};
+
+    // 모달 폼 닫기
+    const handleClose = () => {setOpen(false)};
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCar({...car, [event.target.name]: event.target.value});
+    }
+
     return (
-        <></>
+        <>
+            <button onClick={handleClickOpen}>New Car</button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>New car</DialogTitle>
+                <DialogContent>
+                    <input placeholder="Brand" name="brand" value={car.brand} onChange={handleChange}/><br/>
+                    <input placeholder="Model" name="model" value={car.model} onChange={handleChange}/><br/>
+                    <input placeholder="Color" name="color" value={car.color} onChange={handleChange}/><br/>
+                    <input placeholder="Year" name="modelYear" value={car.modelYear} onChange={handleChange}/><br/>
+                    <input placeholder="Reg.nr" name="registrationNumber" value={car.registrationNumber} onChange={handleChange}/><br/>
+                    <input placeholder="Price" name="price" value={car.price} onChange={handleChange}/><br/>
+                </DialogContent>
+                <DialogActions>
+                    <button onClick={handleClose}>Cancel</button>
+                    <button onClick={handleClose}>Save</button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 }
 
