2103e9b3076d268d89df9b1d20cf7a5fde1e2d86
khg <winquake3@hanmail.net>
2025-09-19 17:45:51 +0900

    수정 기능 중 - 컴포넌트에 자동차 객체 값이 채워지도록

--- React/carfront/src/components/EditCar.tsx
+++ React/carfront/src/components/EditCar.tsx
@@ -3,6 +3,11 @@ import {Car, CarResponse } from '../types';
 import { Dialog } from "@mui/material/Dialog";
 import { DialogActions } from "@mui/material/DialogActions";
 import { DialogTitle } from "@mui/material/DialogTitle";
+import CarDialogContent from "./CarDialogContent";
+
+const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
+    setCar({...car, [event.target.name]: event.target.validationMessage});
+}
 
 type FormProps = {
     cardata : CarResponse;
@@ -19,7 +24,18 @@ function EditCar({cardata}: FormProps) {
         price: 0
     });
 
-    const handleClickOpen = () => { setOpen(true); };
+    const handleClickOpen = () => { 
+        setCar({
+            brand: cardata.brand,
+            model: cardata.model,
+            color: cardata.color,
+            registrationNumber: cardata.registrationNumber,
+            modelYear: cardata.modelYear,
+            price: cardata.price
+        });
+
+        setOpen(true); 
+    };
     const handleClose = () => { setOpen(false); };
     const handleSave = () => { setOpen(false); };
 
@@ -27,6 +43,8 @@ function EditCar({cardata}: FormProps) {
         <>
             <button onClick={handleClickOpen}>Edit</button>
             <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Edit car</DialogTitle>
+                <CarDialogContent car={car} handleChange={handleChange}/>
                 <DialogActions>
                     <button onClick={handleClose}>Cancel</button>
                     <button onClick={handleSave}>Save</button>
