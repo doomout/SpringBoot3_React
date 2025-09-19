7666f9f22e48c69c91e941a4fa404d60e86d80e0
khg <winquake3@hanmail.net>
2025-09-20 01:04:36 +0900

    수정 기능 완성

--- React/carfront/src/components/EditCar.tsx
+++ React/carfront/src/components/EditCar.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-import type { Car, CarResponse } from '../types';
+import type { Car, CarResponse, CarEntry } from '../types';
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogTitle from "@mui/material/DialogTitle";
 import CarDialogContent from "./CarDialogContent";
+import { updateCar } from "../api/carapi";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type FormProps = {
     cardata : CarResponse;
 }
+
 
 function EditCar({cardata}: FormProps) {
     const [open, setOpen] = useState(false);
@@ -32,13 +35,39 @@ function EditCar({cardata}: FormProps) {
 
         setOpen(true); 
     };
+    
     const handleClose = () => { setOpen(false); };
-    const handleSave = () => { setOpen(false); };
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
-        setCar({...car, [event.target.name]: event.target.value});
+    
+    const handleSave = () => {
+        const url = cardata._links.self.href;
+        const carEntry: CarEntry = {car, url}
+        mutate(carEntry);
+        setCar({ brand:'', model:'', color:'', registrationNumber:'', modelYear: 0, price: 0 });
+        setOpen(false);
     }
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
+        const { name, value }  = event.target;
+        setCar({
+            ...car, 
+            [name]: name === "modelYear" || name === "price" ? Number(value) : value,
+        });
+    }
+
+    // 쿼리 클라이언트 가져오기
+    const queryClient = useQueryClient();
+
+    // useMutation 훅 이용
+    const { mutate }= useMutation<CarResponse, Error, CarEntry>({
+        mutationFn: updateCar,
+        onSuccess: () => { 
+            queryClient.invalidateQueries({ queryKey: ["cars"]}); 
+        },
+        onError: (err: Error) => { 
+            console.error(err.message); 
+        }
+    });
+    
     return (
         <>
             <button onClick={handleClickOpen}>Edit</button>
