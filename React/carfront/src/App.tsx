e13ab9e1327b4932bde9bcd99b85cea58877167f
khg <winquake3@hanmail.net>
2025-09-25 05:02:01 +0900

    로그인 컴포넌트 완성

--- React/carfront/src/App.tsx
+++ React/carfront/src/App.tsx
@@ -4,7 +4,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Carlist from './components/CarList';
+import Login from "./components/Login";
 
 const queryClient = new QueryClient();
 
@@ -20,7 +20,7 @@ function App() {
           </Toolbar>
         </AppBar>
         <QueryClientProvider client={queryClient}>
-          <Carlist />
+          <Login />
         </QueryClientProvider>
     </Container>
   );
---
+++ React/carfront/src/components/Login.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import axios from "axios";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import CarList from "./CarList";
+
+type User = {
+    username: string;
+    password: string;
+}
+
+function Login() {
+    const [user, setUser] = useState<User>({
+        username: '',
+        password: ''
+    });
+    
+    const [isAuthenticated, setAuth] = useState(false);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUser({...user, [event.target.name] : event.target.value});
+    }
+
+    const handleLogin = () => {
+        axios.post(import.meta.env.VITE_API_URL + "/login", user, {
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then(res => {
+            const jwtToken = res.headers.authorization;
+            if(jwtToken !== null) {
+                sessionStorage.setItem("jwt", jwtToken);
+                setAuth(true);
+            }
+        })
+        .catch(err => console.error(err));
+    }
+
+    if(isAuthenticated) {
+        return <CarList />
+    }
+    else {
+        return (
+            <Stack spacing={2} alignItems="center" mt={2}>
+                <TextField 
+                    name="username"
+                    label="Username"
+                    onChange={handleChange} />
+                <TextField 
+                    type="password"
+                    name="password"
+                    label="Password"
+                    onChange={handleChange} />
+                <Button 
+                    variant="outlined"
+                    color="primary"
+                    onClick={handleLogin} >
+                        Login
+                </Button>
+            </Stack>
+        );
+    }
+}
+
+export default Login;
