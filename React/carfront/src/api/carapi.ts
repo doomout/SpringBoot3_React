d8d529031afa6dd6ac367348d90914281d94ea6c
khg <winquake3@hanmail.net>
2025-09-26 02:26:25 +0900

    토큰 중복 코드 리팩토링

--- React/carfront/src/api/carapi.ts
+++ React/carfront/src/api/carapi.ts
@@ -1,48 +1,39 @@
 import  type { CarResponse, Car, CarEntry } from "../types";
 import axios from 'axios';
+import type { AxiosRequestConfig } from 'axios';
 
-export const getCars = async (): Promise<CarResponse[]> => {
+const getAxiosConfig = (): AxiosRequestConfig => {
     const token = sessionStorage.getItem("jwt");
 
-    const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`, {
-        headers: { 'Authorization': token }
-    });
+    return {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token,
+        }
+    };
+};
+
+export const getCars = async (): Promise<CarResponse[]> => {
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/cars`, getAxiosConfig());
     return response.data._embedded.cars;
 }
 
 export const deleteCar = async (link: string): Promise<CarResponse> =>  {
-    const token = sessionStorage.getItem("jwt");
-    const response = await axios.delete(link, {
-        headers: { 'Authorization': token }
-    });
+    const response = await axios.delete(link, getAxiosConfig());
     return response.data;
 }
 
 
 // 새 자동차 추가
 export const addCar = async (car: Car): Promise<CarResponse> => {
-    const token = sessionStorage.getItem("jwt");
-
-    const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/cars`, car, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': token
-        },
-    });
+    const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/cars`, car, getAxiosConfig());
 
     return response.data;
 }
 
 // updateCar 함수를 추가
 export const updateCar = async (carEntry: CarEntry):Promise<CarResponse> => {
-    const token = sessionStorage.getItem("jwt");
-
-    const response = await axios.put(carEntry.url, carEntry.car, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': token
-        },
-    });
+    const response = await axios.put(carEntry.url, carEntry.car, getAxiosConfig());
 
     return response.data;
 }
